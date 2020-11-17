8a044d29d0e72a82ac80060034efa5db895a9bc9
vzaremba <zarwlad@yandex.ru>
2020-11-17 12:36:58 +0300

    Initial commit

---
+++ src/main/java/ru/zarwlad/zarwladbot/ZarwladBotApplication.java
@@ -0,0 +1,13 @@
+package ru.zarwlad.zarwladbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarwladBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarwladBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zarwlad/zarwladbot/ZarwladBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zarwlad.zarwladbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarwladBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
