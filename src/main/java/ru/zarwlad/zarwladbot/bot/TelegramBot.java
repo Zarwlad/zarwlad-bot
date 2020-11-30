5b3454c9e7546de9bec57ac64b98eb861a4e4fae
vzaremba <zarwlad@yandex.ru>
2020-11-30 18:51:08 +0300

    Initial commit: added TelegramBot, Message routing

--- src/main/java/ru/zarwlad/zarwladbot/bot/TelegramBot.java
+++ src/main/java/ru/zarwlad/zarwladbot/bot/TelegramBot.java
@@ -1,9 +1,6 @@
 package ru.zarwlad.zarwladbot.bot;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,9 +9,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${zarwlad.telegram.api.bot-token}")
     private String botToken;
